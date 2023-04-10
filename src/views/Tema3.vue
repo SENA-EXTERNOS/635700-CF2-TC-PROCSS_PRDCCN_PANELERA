51070cc592bb19623af999c44b8a84b1fd8627fc
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-04-10 15:30:39 -0500

    Videos

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -16,7 +16,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/f_Vk-TuAcFI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
 
     .tarjeta.tarjeta-amarillo.p-4.p-md-5.mb-5
